b24ffcb5c1657540538ced7f4cfd770fd65cdc3f
Ragan Lee <raganwlee@icloud.com>
2025-10-16 14:06:12 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React Website!</h1>
+      <p>This is my first React app.</p>
+    </div>
+  );
+}
+
+export default App;
